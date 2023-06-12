2836b436b80723d2141d5f67d6d42e32845d6396
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-06-12 19:29:52 -0300

    create auth module with submodules, components and routes
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ src/app/auth/register/register.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './containers/register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+
+export const ROUTES: Routes = [{ path: '', component: RegisterComponent }];
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(ROUTES)],
+})
+export class RegisterModule {}
