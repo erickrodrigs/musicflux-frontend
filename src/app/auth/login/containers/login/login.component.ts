618d92d66043f28c656197baec8a7470729f5779
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-06-14 11:25:21 -0300

    add loading spinner when clicking to sign in
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/app/auth/login/containers/login/login.component.ts
+++ src/app/auth/login/containers/login/login.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from '../../../../auth/services/auth.service';
 })
 export class LoginComponent {
   errorMessage = '';
+  loading = false;
 
   form = this.formBuilder.group({
     username: ['', Validators.required],
@@ -34,13 +35,19 @@ export class LoginComponent {
     return this.errorMessage.length > 0;
   }
 
+  get isNotLoading() {
+    return !this.loading;
+  }
+
   onSubmit() {
     if (this.form.invalid) return;
 
     const { username, password } = this.form.value;
+    this.loading = true;
     this.authService
       .login(username as string, password as string)
       .subscribe((response) => {
+        this.loading = false;
         if (response.error) {
           this.errorMessage = response.error;
           return;
