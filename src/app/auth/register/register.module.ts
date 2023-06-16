1147bea5fe8dd51ea921c609fdc1c29ed24a1f60
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-06-16 08:53:00 -0300

    implement register component
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './containers/register/register.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RegisterComponent } from './containers/register/register.component';
 
 export const ROUTES: Routes = [{ path: '', component: RegisterComponent }];
 
 @NgModule({
   declarations: [RegisterComponent],
-  imports: [CommonModule, RouterModule.forChild(ROUTES)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+  ],
 })
 export class RegisterModule {}
