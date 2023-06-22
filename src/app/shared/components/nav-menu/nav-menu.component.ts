798fbf3196d0ac3b6e5de790d2d0ea343426b950
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-06-22 16:18:35 -0300

    create nav menu component
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ src/app/shared/components/nav-menu/nav-menu.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'nav-menu',
+  styleUrls: ['nav-menu.component.scss'],
+  templateUrl: 'nav-menu.component.html',
+  standalone: true,
+  imports: [CommonModule, MatSidenavModule, RouterModule, MatIconModule],
+})
+export class NavMenuComponent {
+  menuOptions = [
+    { iconName: 'home', name: 'Home', link: '' },
+    { iconName: 'search', name: 'Search', link: '' },
+    { iconName: 'library_music', name: 'Library', link: '' },
+    { iconName: 'history', name: 'History', link: '' },
+  ];
+}
