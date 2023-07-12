b9b758b8577e32cfb55638113515722517226561
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-07-11 21:25:54 -0300

    fix app test
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 
 describe('AppComponent', () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, AuthModule.forRoot()],
       declarations: [AppComponent],
     })
   );
