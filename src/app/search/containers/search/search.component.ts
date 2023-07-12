db58f6e6f91fa893dbb2a357e4a54af3f29e1371
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-07-12 13:40:57 -0300

    add search page layout
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ src/app/search/containers/search/search.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'search',
+  templateUrl: 'search.component.html',
+  styleUrls: ['search.component.scss'],
+})
+export class SearchComponent {
+  search = '';
+
+  filterOptions = [
+    { name: 'Artist', selected: false },
+    { name: 'Album', selected: false },
+    { name: 'Track', selected: false },
+    { name: 'Playlist', selected: false },
+  ];
+  artists = Array.from({ length: 2 }).map(() => ({
+    coverUrl: '',
+    name: 'Artist name',
+  }));
+  albums = Array.from({ length: 2 }).map(() => ({
+    coverUrl: '',
+    name: 'Album title',
+    description: 'Artist name',
+  }));
+  tracks = Array.from({ length: 8 }).map(() => ({
+    coverUrl: '',
+    name: 'Track title',
+    description: 'Artist name',
+  }));
+  playlists = Array.from({ length: 2 }).map(() => ({
+    coverUrl: '',
+    name: 'Playlist name',
+  }));
+
+  get showAll() {
+    return this.filterOptions.every(({ selected }) => !selected);
+  }
+
+  get showArtists() {
+    return (
+      this.showAll ||
+      this.filterOptions.find((option) => option.name === 'Artist')?.selected
+    );
+  }
+
+  get showAlbums() {
+    return (
+      this.showAll ||
+      this.filterOptions.find((option) => option.name === 'Album')?.selected
+    );
+  }
+
+  get showTracks() {
+    return (
+      this.showAll ||
+      this.filterOptions.find((option) => option.name === 'Track')?.selected
+    );
+  }
+
+  get showPlaylists() {
+    return (
+      this.showAll ||
+      this.filterOptions.find((option) => option.name === 'Playlist')?.selected
+    );
+  }
+
+  onFilterOptionSelectionChange(option: { name: string; selected: boolean }) {
+    option.selected = !option.selected;
+  }
+
+  onSearch() {
+    console.log(`searching for ${this.search}...`);
+  }
+}
