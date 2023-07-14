1a5ad4ee48ef942e9c4ca46ade2e4be8e495a78b
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-07-14 14:54:50 -0300

    add artist page header component
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/app/artist/artist.module.ts
+++ src/app/artist/artist.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AuthModule } from '../auth/auth.module';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { ArtistComponent } from './containers/artist/artist.component';
+import { ArtistHeaderComponent } from './components/artist-header/artist-header.component';
 
 const routes: Routes = [
   { path: ':id', canActivate: [AuthGuard], component: ArtistComponent },
 ];
 
 @NgModule({
-  declarations: [ArtistComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), AuthModule.forRoot()],
+  declarations: [ArtistComponent, ArtistHeaderComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    AuthModule.forRoot(),
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class ArtistModule {}
