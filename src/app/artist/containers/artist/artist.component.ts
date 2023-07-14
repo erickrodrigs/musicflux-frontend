3cf6c5562be159498972213d483e14488da92520
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-07-13 21:20:32 -0300

    create artist module
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ src/app/artist/containers/artist/artist.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'artist',
+  templateUrl: 'artist.component.html',
+  styleUrls: ['artist.component.scss'],
+})
+export class ArtistComponent {}
