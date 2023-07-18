17ae26ce9850a027d86532ba9e9c9aef44bbd052
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-07-18 14:44:07 -0300

    add track component
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

---
+++ src/app/track/track.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { TrackComponent } from './components/track/track.component';
+
+@NgModule({
+  declarations: [TrackComponent],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  exports: [TrackComponent],
+})
+export class TrackModule {}
