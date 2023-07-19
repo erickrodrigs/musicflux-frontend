029c8c8bb8176e77f99e635712068b40241e3f71
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-07-19 19:44:02 -0300

    add album-header component
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/app/album/album.module.ts
+++ src/app/album/album.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AuthModule } from '../auth/auth.module';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AlbumComponent } from './containers/album/album.component';
+import { AlbumHeaderComponent } from './components/album-header/album-header.component';
 
 const routes: Routes = [
   { path: ':id', canActivate: [AuthGuard], component: AlbumComponent },
 ];
 
 @NgModule({
-  declarations: [AlbumComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), AuthModule.forRoot()],
+  declarations: [AlbumComponent, AlbumHeaderComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    AuthModule.forRoot(),
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class AlbumModule {}
