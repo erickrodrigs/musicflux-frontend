5314b6e181278488ffb600fd41aa523c89849c21
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-07-18 21:20:55 -0300

    add artist service
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/app/artist/containers/artist/artist.component.ts
+++ src/app/artist/containers/artist/artist.component.ts
@@ -1,48 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin, of, switchMap } from 'rxjs';
+import { ArtistService } from '../../services/artist.service';
 import { Artist } from '../../models/artist';
-import { Track } from '../../../track/models/track';
 import { Album } from '../../../album/models/album';
+import { Track } from '../../../track/models/track';
 
 @Component({
   selector: 'artist',
   templateUrl: 'artist.component.html',
   styleUrls: ['artist.component.scss'],
 })
-export class ArtistComponent {
-  artist: Artist = {
-    id: 1,
-    name: 'Depeche Mode',
-    biography: 'The best band ever',
-    photoUrl:
-      'https://www.antena1rio.com.br/wp-content/uploads/6b6abc6b506c5d8bd3bc1dd8c17a24e4.png',
-  };
+export class ArtistComponent implements OnInit {
+  artist: Artist = { id: 1, name: '', biography: '', photoUrl: '' };
+  albums: Album[] = [];
+  topTracks: Track[] = [];
 
-  albums: Album[] = [
-    {
-      id: 1,
-      title: 'Music For The Masses',
-      coverUrl:
-        'https://i.scdn.co/image/ab67616d0000b2738636ea09d374ec9937e81388',
-      releaseDate: new Date(),
-      artists: [{ id: 1, name: 'Depeche Mode' }],
-    },
-  ];
+  constructor(
+    private readonly artistService: ArtistService,
+    private readonly route: ActivatedRoute
+  ) {}
 
-  tracks: Track[] = [
-    {
-      id: 1,
-      title: 'Never Let Me Down Again',
-      length: 287,
-      numberOfPlays: 6975832,
-      liked: true,
-      artists: [{ id: 1, name: 'Depeche Mode' }],
-      album: {
-        id: 1,
-        title: 'Music For The Masses',
-        coverUrl:
-          'https://i.scdn.co/image/ab67616d0000b2738636ea09d374ec9937e81388',
-      },
-      genres: ['Synth-pop'],
-    },
-  ];
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id') as unknown as number;
+    this.artistService
+      .getById(id)
+      .pipe(
+        switchMap((artist) =>
+          forkJoin({
+            artist: of(artist),
+            albums: this.artistService.getAlbums(artist.id),
+            topTracks: this.artistService.getTopTracks(artist.id),
+          })
+        )
+      )
+      .subscribe(({ artist, albums, topTracks }) => {
+        this.artist = artist;
+        this.albums = albums;
+        this.topTracks = topTracks;
+      });
+  }
 }
