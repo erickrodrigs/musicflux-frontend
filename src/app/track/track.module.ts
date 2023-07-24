5c5e36e396b630312dc7df8889eac362078ce9f4
erickrodrigs <erick.rodrigues.santana14@gmail.com>
2023-07-24 16:36:08 -0300

    add track service
    Signed-off-by: erickrodrigs <erick.rodrigues.santana14@gmail.com>

--- src/app/track/track.module.ts
+++ src/app/track/track.module.ts
@@ -4,10 +4,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { TrackComponent } from './components/track/track.component';
+import { TrackService } from './services/track.service';
 
 @NgModule({
   declarations: [TrackComponent],
   imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  providers: [TrackService],
   exports: [TrackComponent],
 })
 export class TrackModule {}
